302b9c6ae15901ac005ee1dd0584413022be0ed0
lihaong <123200054@student.upnyk.ac.id>
2021-07-29 14:00:29 +0700

    first commit

---
+++ Aplikasi/oyi-store/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">Oyi Oke Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        Copyright
+      </footer>
+    </div>
+  );
+}
+
+export default App;
